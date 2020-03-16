015d526e125d6a0800004d75e1a392ec6072c388
Jorge Lopez <JorgeLopez5252@gmail.com>
2020-03-16 14:25:00 -0700

    console logged click event to get javascript started

---
+++ script.js
@@ -0,0 +1,14 @@
+const startButton = document.getElementById('start-btn')
+
+startButton.addEventListener('click', startGame)
+
+function startGame() {
+ console.log('Started')
+}
+function setNextQuestion() {
+
+}
+
+function selectAnswer(){
+
+}
