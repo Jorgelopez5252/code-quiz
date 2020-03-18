43ee066cb269c8072bf86e53d84136ad0ad127d5
Jorge Lopez <JorgeLopez5252@gmail.com>
2020-03-17 22:44:42 -0700

    able to generate the question to appear

--- script.js
+++ script.js
@@ -1,18 +1,35 @@
 const startButton = document.getElementById('start-btn')
 const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
 
-const shuffledQuestions, currentQuestionsIndex
+let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startGame)
 
 function startGame() {
  console.log('Started')
  startButton.classList.add('hide')
- shuffledQuestions = questions.sort(() => Math.random)
+ shuffledQuestions = questions.sort(() => Math.random() - .5)
+ currentQuestionIndex = 0
  questionContainerElement.classList.remove('hide')
  setNextQuestion()
 }
+
 function setNextQuestion() {
+showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if(answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+    })
 
 }
 
@@ -21,8 +38,8 @@ function selectAnswer(){
 }
 const questions = [
     {
-        question:'What is 2+2',
-        answers:[
+        question: 'What is 2 + 2',
+        answers: [
             {text: '4', correct:true},
             {text: '22', correct:false}
         ]
