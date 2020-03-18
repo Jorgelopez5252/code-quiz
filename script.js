fab95a2ed03402a95c15de223791d2e753695864
Jorge Lopez <JorgeLopez5252@gmail.com>
2020-03-17 22:27:47 -0700

    started to add functionaltiy to my app

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 const startButton = document.getElementById('start-btn')
+const questionContainerElement = document.getElementById('question-container')
+
+const shuffledQuestions, currentQuestionsIndex
 
 startButton.addEventListener('click', startGame)
 
 function startGame() {
  console.log('Started')
+ startButton.classList.add('hide')
+ shuffledQuestions = questions.sort(() => Math.random)
+ questionContainerElement.classList.remove('hide')
+ setNextQuestion()
 }
 function setNextQuestion() {
 
@@ -12,3 +19,12 @@ function setNextQuestion() {
 function selectAnswer(){
 
 }
+const questions = [
+    {
+        question:'What is 2+2',
+        answers:[
+            {text: '4', correct:true},
+            {text: '22', correct:false}
+        ]
+    }
+]
